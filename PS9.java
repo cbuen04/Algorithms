75f96168e0c2c637065d1a50efbc2c75dc4c9214
cbuen04 <charlybueno2001@gmail.com>
2022-04-30 11:04:22 -0600

    pq working, bugs still in algo

--- PS9.java
+++ PS9.java
@@ -6,12 +6,12 @@ public class PS9 {
     private static int maxFlow = 0;
     private static int flowStoT = 0; // should be max flow
     private static int capTtoS = 0; // should be max flow
+    private static int saturatedEdges = 0;
     private static int[][] capacities;
     private static int[][] flow;
     private static Vertex[][] residualGraph;
     private static ArrayList<Integer> flowGained = new ArrayList<>();
     private static ArrayList<Integer> setS = new ArrayList<>();
-    private static ArrayList<Integer> setT = new ArrayList<>();
 
 
     public static void main(String[] args){
@@ -41,27 +41,31 @@ public class PS9 {
         for(int i = 0; i < numOfVerts; i++){
             for(int j = 0; j < numOfVerts; j++){
                 int cap = Math.max(capacities[i][j], flow[i][j]);
-                Vertex curr = new Vertex(i, null, -1 cap);
+                Vertex curr = new Vertex(i, j, cap);
                 curr.setFlow(flow[i][j]);
                 residualGraph[i][j] = curr;
             }
         }
 
+
         while(pathFromStoT(source, target)){
-            System.out.println("yes");
-            if(maxFlow > 20){
-                System.out.println(maxFlow);
+            for(int i = 0; i < numOfVerts; i ++){
+                for(int j = 0; j < numOfVerts; j++){
+                    residualGraph[i][j].prevParent = null;
+                    residualGraph[i][j].pathLength = Integer.MAX_VALUE;
+                }
             }
         }
 
 
-//        Collections.sort(setS);
-//        Collections.sort(setT);
-
-        System.out.println(flowGained);
+        //Collections.sort(setS);
+        for(int i = 0; i < flowGained.size()-1; i++){
+            System.out.print(flowGained.get(i) + " ");
+        }
+        System.out.println(flowGained.get(flowGained.size()-1));
         System.out.println(maxFlow);
 //        //A single integer indicating the number of saturated edges in the s-t maximum flow.
-//        System.out.println(setS.size());
+//        System.out.println(saturatedEdges);
 //        //A sorted (increasing) sequence of integers indicating the indices of the vertices in S in the s-t cut.
 //        System.out.println(setS);
 //        //A single integer indicating the total flow across the edges from S to T.  (Note that S and T are sets of vertices in the min-cut.)
@@ -78,47 +82,65 @@ public class PS9 {
 
         int cap = Integer.MAX_VALUE;
         boolean[] visited = new boolean[numOfVerts];
-        int[] currentLength = new int[numOfVerts];
-        int[] prevPath = new int[numOfVerts];
 
         for(int i = 0; i < numOfVerts; i++){
-            currentLength[i] = Integer.MAX_VALUE;
             visited[i] = false;
         }
         //do a bfs using a priority queue where lighter weights are favored
         PriorityQueue<Vertex> pq = new PriorityQueue<>();
         for(int i = 0; i < numOfVerts; i++){
             Vertex start = residualGraph[source][i];
+            start.prevParent = null; // set parent to null, this is our root
             pq.add(start);
         }
         visited[source] = true;
-        currentLength[source] = 0;
-        prevPath[source] = -1;
 
-
+        Vertex pathVertex = null; // this vertex will hold the target path
         while(!pq.isEmpty()){
             Vertex current = pq.poll();
-            int newWeight = currentLength[current.start] + 1;
-            if(currentLength[current.dest] > newWeight && current.feasibleFlow > 0){
+
+            int newWeight = 1; // initialize path length from source for root vertex with no parent
+            if(current.prevParent != null){ // safety clause
+                newWeight = current.prevParent.pathLength + 1;
+            }
+
+            if(current.pathLength > newWeight && current.feasibleFlow > 0 && !visited[current.dest]){
                 if(cap > current.feasibleFlow){
                     cap = current.feasibleFlow; // sets the maximum possible flow that can travel on this path
-
                 }
-                currentLength[current.dest] = newWeight;
-                prevPath[current.dest] = current.start;
-                //TODO: maybe add a check to see if dest == sink and return early
+                current.pathLength = newWeight;
 
+                if(current.dest == sink){ // if current is the sink we can break out of the loop
+                    pathVertex = current;
+                    visited[sink] = true;
+                    break;
+                }
+                //TODO: maybe add a check to see if dest == sink and return early
                 if(!visited[current.dest]){
                     for(int i = 0; i < numOfVerts; i++){
                         Vertex next = residualGraph[current.dest][i];
+                        next.prevParent = current;
                         pq.add(next);
                     }
+
                     visited[current.dest] = true;
                 }
             }
         }
 
         if(!visited[sink]){ // if dijkstras couldn't reach the sink then break out there is no more work that we can do
+            for(int i = 0; i < numOfVerts; i++){
+                if(visited[i]){
+                    setS.add(i);
+                    for(int j = 0; j < numOfVerts; j++){
+                        if(residualGraph[i][j].feasibleFlow == 0 && residualGraph[i][j].capacity > 0){ // this is a saturated edge
+                            saturatedEdges++;
+                            flowStoT += residualGraph[i][j].flow;  // total flow across s to t
+                            capTtoS += residualGraph[j][i].capacity;
+                        }
+                    }
+                }
+            }
             return false;
         }
 
@@ -127,7 +149,12 @@ public class PS9 {
         //update residual graph
         //pull the path
         ArrayList<Integer> finalShortestPath = new ArrayList<>();
-        generatePath(prevPath, sink, finalShortestPath);
+        finalShortestPath.add(pathVertex.dest);
+
+        while(pathVertex != null){
+            finalShortestPath.add(pathVertex.start);
+            pathVertex = pathVertex.prevParent;
+        }
 
         Collections.reverse(finalShortestPath);
         for(int i = 0; i < finalShortestPath.size()-1; i++){
@@ -135,8 +162,8 @@ public class PS9 {
                 //add the capped flow to the graph switching both i,j and j,i in the resid graph
                 int x = finalShortestPath.get(i);
                 int y = finalShortestPath.get(j);
-                residualGraph[x][y].setFlow(cap + residualGraph[x][y].flow); //TODO: logic sound but could be wrong debug
-                residualGraph[y][x].setFlow(residualGraph[y][x].flow - cap);
+                residualGraph[x][y].setFlow(cap); //TODO: I think this works now
+                residualGraph[y][x].setFlow(-cap);
             }
         }
 
@@ -147,13 +174,6 @@ public class PS9 {
         return true;
     }
 
-    public static void generatePath(int[] paths, int index, ArrayList<Integer> path){
-        path.add(0,index);
-        if(paths[index] == -1){
-            return;
-        }
-        generatePath(paths, paths[index], path);
-    }
 
     public static void StoTCut(){
         //search all verts for saturated edges
@@ -174,20 +194,17 @@ public class PS9 {
 
         private int start;
         private int dest;
-
-        private int vertexName;
         public int pathLength;
         public Vertex prevParent;
         private int capacity;
         private int flow;
         private int feasibleFlow;
 
-        public Vertex(int vertex, int c) {
-            start = 0;
-            dest = 0;
-            vertexName = vertex;
+        public Vertex(int s, int d, int c) {
+            start = s;
+            dest = d;
             capacity = feasibleFlow = c;
-            pathLength = -1;
+            pathLength = Integer.MAX_VALUE;
             flow = 0;
 
         }
@@ -197,34 +214,33 @@ public class PS9 {
             feasibleFlow = capacity - flow;
         }
 
-//        private int smallestParent(Vertex o){
-//            //TODO: remove this
-//            System.out.println("confirmed this was used");
-//            Vertex temp1 = prevParent;
-//            Vertex temp2 = o.prevParent;
-//            while(temp1 != null && temp2 != null){
-//                if(temp1.start < temp2.start){
-//                    return -1;
-//                }
-//                if(temp1.start > temp2.start){
-//                    return 1;
-//                }
-//                temp1 = temp1.prevParent;
-//                temp2 = temp2.prevParent;
-//            }
-//            return 0;
-//        }
+        private int smallestParent(Vertex o){
+
+            Vertex temp1 = prevParent;
+            Vertex temp2 = o.prevParent;
+            while(temp1 != null && temp2 != null){
+                if(temp1.start < temp2.start){
+                    return -1;
+                }
+                if(temp1.start > temp2.start){
+                    return 1;
+                }
+                temp1 = temp1.prevParent;
+                temp2 = temp2.prevParent;
+            }
+            return 0;
+        }
 
         @Override
         public int compareTo(Vertex o) {
 
             //first rule, smallest path
-//            if (pathLength < o.pathLength) {
-//                return -1;
-//            }
-//            if (pathLength > o.pathLength) {
-//                return 1;
-//            }
+            if (pathLength < o.pathLength) {
+                return -1;
+            }
+            if (pathLength > o.pathLength) {
+                return 1;
+            }
 
             // second rule, largest feasible flow
             if (feasibleFlow > o.feasibleFlow) {
@@ -242,9 +258,7 @@ public class PS9 {
                 return 1;
             }
 
-            //TODO: remove debug
-           // System.out.println("smallest Parent used");
-            return 0;
+            return smallestParent(o);
         }
 
         @Override
@@ -255,6 +269,18 @@ public class PS9 {
                 }
             }
             return false;
+        }
+
+        @Override
+        public String toString(){
+            StringBuilder sb = new StringBuilder();
+            sb.append(start + "->" + dest);
+            Vertex parent = prevParent;
+            while (parent != null){
+                sb.insert(0, parent.start + "->");
+                parent = parent.prevParent;
+            }
+            return sb.toString();
         }
     }
 }
